b5bcf37009c33214fd37ccbdcd66277013e7b706
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-11-04 13:33:45 -0300

    Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import CarouselHome from "./components/CarouselHome/CarouselHome";
 import NavBar from "./components/NavBar/NavBar"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const App = () => {
   return (
+    <>
     <NavBar/>
+    <CarouselHome/>
+    </>
+    
   )
 }
 
