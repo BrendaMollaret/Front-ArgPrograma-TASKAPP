a648c82b9446bc951365271f6dcd6d4a75dcb1b9
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-11-07 19:19:50 -0300

    .

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,28 +1,26 @@
-import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
-import { Basket, Person } from "react-bootstrap-icons";
-import { useNavigate } from "react-router-dom";
-import ModalAgregarTarea from "../ModalAgregarTarea/ModalAgregarTarea";
 import { useState } from "react";
-import { Task } from "../../types/Task";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap"
+import { Basket, Person } from "react-bootstrap-icons"
+import { useNavigate } from "react-router-dom";
 import { TaskService } from "../../services/TaskService";
+import { Task } from "../../types/Task";
 import { toast } from "react-toastify";
+import ModalAgregarTarea from "../ModalAgregarTarea/ModalAgregarTarea";
 
 const NavBar = () => {
 
-    const navigate = useNavigate();
-    const [showModal, setShowModal] = useState(false);
-      
-    
-    const handleShowModal = () => {
-      setShowModal(true);
-    };
-  
-    const handleCloseModal = () => {
-      setShowModal(false);
-    };
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false);
 
-    
-    //Agregar nueva tarea
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+      //Agregar nueva tarea
 const createTask = async (newTask: Task) => {
   try {
     const result = await TaskService.createTask(newTask);
@@ -44,69 +42,66 @@ const createTask = async (newTask: Task) => {
   }
 };
 
+    return (
+
+        <>
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <Navbar.Brand >Desarrollo en Argentina</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
     
-  
+              <Nav className="me-auto">
 
-  return (
-
-    <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand >Desarrollo en Argentina</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-
-          <Nav className="me-auto">
-          <Nav.Link onClick={() => navigate('/')}> Inicio </Nav.Link>
-
-            <NavDropdown title="Tareas" id="basic-nav-dropdown">
-              <NavDropdown.Item onClick={() => navigate('/PORHACER')}>Por hacer</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                En producción
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Por testear</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Completada
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            {/* ---------- Agregar una nueva tarea -----------*/}
-            <Nav.Link onClick={handleShowModal}>Agregar tarea</Nav.Link>
-          </Nav>
-
-        <Nav className="d-none d-md-flex ms-auto">
-            <Nav.Link href="#carrito">
-                <Basket />
-            </Nav.Link>
-
-            <Nav.Link href="#usuario">
-                <Person />
-            </Nav.Link>
-        </Nav>
-
-        <div className="d-md-none">
-            <ul className="navbar-nav me-auto-mb-2 mb-md-0">
-                <li className="nav-item">
-                    <a className="nav-link" href="#ticket">Ticket</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#perfil">Perfil</a>
-                </li>
-            </ul>
-        </div>
-
-
-        </Navbar.Collapse>
-      </Container>
-      
-    </Navbar>
-    <ModalAgregarTarea showModal={showModal} handleClose={handleCloseModal} createTask={createTask} />
-    </>
+              <Nav.Link onClick={() => navigate('/')}> Inicio </Nav.Link>
+    
+                <NavDropdown title="Tareas" id="basic-nav-dropdown">
+                  <NavDropdown.Item >Por hacer</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2"> En producción </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Por testear</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4"> Completada </NavDropdown.Item>
+                </NavDropdown>
+    
+                {/* ---------- Agregar una nueva tarea -----------*/}
+                <Nav.Link onClick={handleShowModal}>Agregar tarea</Nav.Link>
+                
+              </Nav>
+    
+            <Nav className="d-none d-md-flex ms-auto">
+                <Nav.Link href="#carrito">
+                    <Basket />
+                </Nav.Link>
+    
+                <Nav.Link href="#usuario">
+                    <Person />
+                </Nav.Link>
+            </Nav>
+    
+            <div className="d-md-none">
+                <ul className="navbar-nav me-auto-mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#ticket">Ticket</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#perfil">Perfil</a>
+                    </li>
+                </ul>
+            </div>
     
     
-    
-  );
-};
+            </Navbar.Collapse>
+          </Container>
+          
+        </Navbar>
+        
+        <ModalAgregarTarea showModal={showModal} handleClose={handleCloseModal} createTask={createTask} />
 
-export default NavBar;
+        </>
+        
+        
+        
+      )
+}
+
+export default NavBar
